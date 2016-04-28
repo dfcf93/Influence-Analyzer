588d070ce5d18676f5b115a6685dc6363504d55a
Daniel Campos <dacamp@microsoft.com>
2016-04-28 15:54:09 -0700

    Update and rename setup1.sh to setup.sh

---
+++ PSQL/setup.sh
@@ -0,0 +1,9 @@
+sudo add-apt-repository ppa:webupd8team/java 
+sudo apt-get upgrade
+sudo apt-get update
+sudo apt-get install oracle-java8-installer oracle-java8-set-default lsscsi xclip
+sudo apt-get install vim curl git python-software-properties python-pip python-dev
+pip install setuptools 
+pip install psycopg2
+#sudo wget -P  /datadrive  http://reddit-data.s3.amazonaws.com/RS_full_corpus.bz2 
+#sudo bunzip2 /datadrive/RS_full_corpus.bz2
