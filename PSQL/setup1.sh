bbfea137772e2eac0ba6e9de037c6c4da899d092
Ubuntu <erasmus@redditdb.nguq43drvj0urgw4u10xhxgowh.dx.internal.cloudapp.net>
2016-04-28 18:37:55 +0000

    added psql stuff

--- PSQL/setup1.sh
+++ PSQL/setup1.sh
@@ -1,8 +1,11 @@
 sudo add-apt-repository ppa:webupd8team/java 
+sudo apt-get upgrade
+sudo apt-get update
 sudo apt-get install oracle-java8-installer oracle-java8-set-default 
 sudo apt-get install libfftw3-bin libfftw3-dev lsscsi 
 sudo apt-get install vim curl git python-software-properties python-pip python-dev
 sudo apt-get install erlang erlang-doc 
+sudo apt-get install postgresql postgresql-contrib
 pip install setuptools 
 pip install psycopg2
 sudo wget -P  /datadrive  http://reddit-data.s3.amazonaws.com/RS_full_corpus.bz2 
